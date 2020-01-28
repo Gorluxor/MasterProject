70b73a5a4c7da965f9af579feca5fdb354041871
Astral <andrijacvejic@gmail.com>
2020-01-28 09:58:41 +0100

    FastText proba, svm proba

--- connector/train.py
+++ connector/train.py
@@ -10,7 +10,7 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import classification_report
 import pickle
 
-df = pd.read_csv('datasetReldiS.csv', encoding="utf-8", sep="\t")
+df = pd.read_csv('datasetHr.csv', encoding="utf-8", sep="\t")
 df = df[:100000]
 print(df.head())
 print(df.isnull().sum())
@@ -155,22 +155,22 @@ print(new_classes)
 y_pred = crf.predict(X_test)
 print(metrics.flat_classification_report(y_test, y_pred, labels = new_classes))
 
+#
+# def print_transitions(trans_features):
+#     for (label_from, label_to), weight in trans_features:
+#         print("%-6s -> %-7s %0.6f" % (label_from, label_to, weight))
+# print("Top likely transitions:")
+# print_transitions(Counter(crf.transition_features_).most_common(20))
+# print("\nTop unlikely transitions:")
+# print_transitions(Counter(crf.transition_features_).most_common()[-20:])
+#
+# def print_state_features(state_features):
+#     for (attr, label), weight in state_features:
+#         print("%0.6f %-8s %s" % (weight, label, attr))
+# print("Top positive:")
+# print_state_features(Counter(crf.state_features_).most_common(30))
+# print("\nTop negative:")
+# print_state_features(Counter(crf.state_features_).most_common()[-30:])
 
-def print_transitions(trans_features):
-    for (label_from, label_to), weight in trans_features:
-        print("%-6s -> %-7s %0.6f" % (label_from, label_to, weight))
-print("Top likely transitions:")
-print_transitions(Counter(crf.transition_features_).most_common(20))
-print("\nTop unlikely transitions:")
-print_transitions(Counter(crf.transition_features_).most_common()[-20:])
-
-def print_state_features(state_features):
-    for (attr, label), weight in state_features:
-        print("%0.6f %-8s %s" % (weight, label, attr))
-print("Top positive:")
-print_state_features(Counter(crf.state_features_).most_common(30))
-print("\nTop negative:")
-print_state_features(Counter(crf.state_features_).most_common()[-30:])
-
-filename = "model.sav"
+filename = "modelHr500.sav"
 pickle.dump(crf, open(filename, 'wb'))
--- converting_rs_legal_acts_to_akoma_ntoso/ner_try/fastTextTransformer.py
+++ converting_rs_legal_acts_to_akoma_ntoso/ner_try/fastTextTransformer.py
@@ -3,24 +3,38 @@
 import fasttext.util
 
 def writeToDataset(fastText):
-    file = open("fastnumdataset.csv", mode="w+", encoding="utf8")
-    filecsv = open("dataset.csv", mode="r+", encoding="utf8")
+    delimiter = "\t"
+    file = open("datasetReldiSw2v.csv", mode="w+", encoding="utf8")
+    filecsv = open("datasetReldiS.csv", mode="r+", encoding="utf8")
 
     data = filecsv.readlines()
     dataClass = list()
+    dataSequence = list()
+    for i in range(0,len(data)):
+        if i==0:
+            continue
+        spliter = data[i].split(delimiter)
+        vector = fastText.get_word_vector(spliter[1].lower())
+        data[i] = vector
+        dataClass.append(spliter[3] + delimiter + spliter[4])
+        dataSequence.append(spliter[0])
 
-    for i in range(len(data)):
-        spliter = data[i].split(";")
-        data[i] = fastText.get_word_vector(spliter[0].lower())
-        dataClass.append(spliter[1])
+    colnames = "Sequence #" + delimiter
+    for i in range(0, 300):
+        colnames = colnames + "word" + str(i + 1) + delimiter
+    colnames = colnames + "PosTag"
 
     lines = []
     for i in range(len(data)):
+        if i == 0:
+            lines.append(colnames + "\n")
+            continue
         wordencoded = data[i].tolist()
         line = ""
-        for j in range(0,len(wordencoded)):
-            line = line + str(wordencoded[j]) + ";"
-        line = line + str(dataClass[i])
+        line = str(dataSequence[i-1]) + delimiter
+        for j in range(0 , len(wordencoded)):
+            line = line + str(wordencoded[j]) + delimiter
+        line = line + str(dataClass[i-1])
         lines.append(line)
     file.writelines(lines)
     return True
@@ -30,6 +44,7 @@ filepath = "../../cc.sh.300.bin" # Download: https://dl.fbaipublicfiles.com/fast
 print("Start Loading")
 ft = fasttext.load_model(filepath)
   #fasttext.util.find_nearest_neighbor() #nece da radi
+
 frrr = ft.get_word_vector("Kosovo")
 print("Ref encodovoana:")
 print(frrr)
