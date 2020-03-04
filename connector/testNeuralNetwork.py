8c2ab801c52b3a286d57837517b1ced7ad5f32fb
Katarina Glorija Grujic <katarina_glorija@Katarinas-MacBook-Air.local>
2020-03-04 18:33:47 +0100

    NER update

--- connector/testNeuralNetwork.py
+++ connector/testNeuralNetwork.py
@@ -1,11 +1,9 @@
+from keras.models import load_model
+
 import pandas as pd
 import numpy as np
 from keras.preprocessing.sequence import pad_sequences
 from sklearn.model_selection import train_test_split
-from keras.models import Model, Input
-from keras.layers import LSTM, Embedding, Dense, TimeDistributed
-from keras.layers import Bidirectional, concatenate, SpatialDropout1D
-
 
 class SentenceGetter(object):
 
@@ -26,10 +24,6 @@ class SentenceGetter(object):
             return s
         except:
             return None
-
-import pathlib
-path = str(pathlib.Path(__file__).parent.absolute()) + "/neuralNetworkModel1.h5"
-print(path)
 
 data = pd.read_csv("datasetReldiS.csv", encoding="latin1", sep="\t")
 
@@ -91,43 +85,13 @@ y = pad_sequences(maxlen=max_len, sequences=y, value=tag2idx["PAD"], padding='po
 X_word_tr, X_word_te, y_tr, y_te = train_test_split(X_word, y, test_size=0.1, random_state=2018)
 X_char_tr, X_char_te, _, _ = train_test_split(X_char, y, test_size=0.1, random_state=2018)
 
-# input and embedding for words
-word_in = Input(shape=(max_len,))
-emb_word = Embedding(input_dim=n_words + 2, output_dim=20,
-                     input_length=max_len, mask_zero=True)(word_in)
-
-# input and embeddings for characters
-char_in = Input(shape=(max_len, max_len_char,))
-emb_char = TimeDistributed(Embedding(input_dim=n_chars + 2, output_dim=10,
-                           input_length=max_len_char, mask_zero=True))(char_in)
-# character LSTM to get word encodings by characters
-char_enc = TimeDistributed(LSTM(units=20, return_sequences=False,
-                                recurrent_dropout=0.5))(emb_char)
-
-# main LSTM
-x = concatenate([emb_word, char_enc])
-x = SpatialDropout1D(0.3)(x)
-main_lstm = Bidirectional(LSTM(units=50, return_sequences=True,
-                               recurrent_dropout=0.6))(x)
-out = TimeDistributed(Dense(n_tags + 1, activation="softmax"))(main_lstm)
-
-model = Model([word_in, char_in], out)
-
-model.compile(optimizer="adam", loss="sparse_categorical_crossentropy")
+model = load_model('neuralNetworkModel1.h5')
 
 model.summary()
-
-history = model.fit([X_word_tr,
-                     np.array(X_char_tr).reshape((len(X_char_tr), max_len, max_len_char))],
-                    np.array(y_tr).reshape(len(y_tr), max_len, 1),
-                    batch_size=32, epochs=10, validation_split=0.1, verbose=1)
-
 
 y_pred = model.predict([X_word_te,
                         np.array(X_char_te).reshape((len(X_char_te),
                                                      max_len, max_len_char))])
-
-model.save(filepath=path)
 
 i = 100
 p = np.argmax(y_pred[i], axis=-1)
@@ -136,5 +100,3 @@ print(30 * "=")
 for w, t, pred in zip(X_word_te[i], y_te[i], p):
     if w != 0:
         print("{:15}: {:5} {}".format(idx2word[w], idx2tag[t], idx2tag[pred]))
-
-
