7e381276a3ee58b7a795af5a935c2ac498a0d365
Gorluxor <cvejicaca@gmail.com>
2020-04-16 21:18:10 +0200

    Fix of github error of not pulling changed references file py

--- Akoma/convertToLatin/Test.py
+++ Akoma/convertToLatin/Test.py
@@ -20,9 +20,9 @@ for i in range(0, len(fajls)):
         try:
             schema.validate('../data/akoma_result/' + fajls[i])
         except Exception as e1:
-            print("Schema validation error :" + fajls[i]  + "MES:" + e1.message)
-            f.write(fajls[i] + " : Not valid with schema + \n")
+            print("Schema validation error :" + fajls[i] + " MES:" + e1.message)
+            f.write(fajls[i] + " : Not valid with schema " + e1.message + "\n")
     except Exception as e:
         print("Not well formed " + fajls[i])
-        f.write(fajls[i] + " : Not well formed xml document +\n")
+        f.write(fajls[i] + " : Not well formed xml document " + "\n")
 f.close()
